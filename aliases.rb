dbcc152e9d6d2d3d735d5b9b3dd4e7c159893e1e
monkey-t <monkey.tosting@gmail.com>
2016-09-15 22:22:25 +0200

    test

--- aliases.rb
+++ aliases.rb
@@ -1,8 +1,8 @@
 objects = {
-    form_name: (xpath, ".//*[@id='container_sign_in']/div/div[1]"),
-    email: (:xpath, ".//*[@id='sign_in_email']"),
-    pass: (:xpath, ".//*[@id='sign_in_password']"),
-    login_btn: (:xpath, ".//*[@id='sign_in_btn']"),
+    form_name: [:xpath, ".//*[@id='container_sign_in']/div/div[1]"],
+    email: [:xpath, ".//*[@id='sign_in_email']"],
+    pass: [:xpath, ".//*[@id='sign_in_password']"],
+    login_btn: [:xpath, ".//*[@id='sign_in_btn']"],
     # чужой комп чекбокс
     # ссыль на регу
     # ссыль на забыли пароль
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,8 +1,14 @@
+require 'rubygems'
 require 'selenium-webdriver'
 require 'rspec'
 require_relative '../../aliases'
 
 
 Given(/^Open the page "([^"]*)"$/) do |arg1|
-  pending
+  @driver = Selenium::WebDriver.for :firefox
+  @driver.get (arg1)
+end
+
+Then(/^I have title "([^"]*)"$/) do |arg1|
+  @driver.title == arg1
 end
